d4665eca500ea594f77e2bb721915b8d295ac9d1
danhNg.8 <105102066+danhNg08101997@users.noreply.github.com>
2023-05-09 10:28:12 +0700

    AuthFilter & Session

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -26,12 +27,16 @@ public class LoginController extends HttpServlet {
 
         boolean isSuccess = userService.checkLogin(email, password);
         if (isSuccess) {
+            HttpSession session = req.getSession();
+            session.setAttribute("email", email);
+            session.setAttribute("password", password);
+            session.setMaxInactiveInterval(30);
+
             String contextPath = req.getContextPath();
             resp.sendRedirect(contextPath + "/");
         } else {
-            PrintWriter writer = resp.getWriter();
-            writer.println("Login Fail!");
-            writer.close();
+            System.out.println("Login Fail");
+            req.getRequestDispatcher("login.jsp").forward(req,resp);
         }
     }
 }
---
+++ src/main/java/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter đã được kích hoạt");
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        if(!req.getServletPath().startsWith("/login")){
+            System.out.println("Filter đang kiểm tra Login");
+            if (req.getSession().getAttribute("email") != null && req.getSession().getAttribute("password") != null){
+                System.out.println("Filter cho phép đăng nhập");
+                filterChain.doFilter(servletRequest, servletResponse);
+            }else {
+                System.out.println("Chưa đăng nhập Filter chuyển đến login page");
+                resp.sendRedirect(req.getContextPath() + "/login");
+            }
+        }else {
+            System.out.println("Login page");
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+//        Filter.super.destroy();
+    }
+}
